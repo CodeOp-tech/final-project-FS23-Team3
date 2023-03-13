d8c0273664536e96a0f8e4d2c7d8e1cf3d126e62
rese-tw <th.warzecha@gmail.com>
2023-03-13 12:28:55 +0100

    owners, pets, auth

--- routes/owners.js
+++ routes/owners.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var models = require("../models");
 
-/* GET users listing. */
+/* GET all owners. */
 router.get('/', async function(req, res, next) {
   try {
     const owners = await models.Owner.findAll();
@@ -11,5 +11,118 @@ router.get('/', async function(req, res, next) {
     res.status(500).send(error);
   }
 });
+
+/* GET one owner with their pets. */
+router.get('/:id', async function(req, res, next) {
+  const { id } = req.params;
+  try {
+    const owner = await models.Owner.findOne({
+      where: {
+        id,
+      },
+      include: models.Pet,
+    });
+    res.send(owner);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/* GET only pets of an owner.*/
+router.get('/:id/pets', async function(req, res, next) {
+  const { id } = req.params;
+  try {
+    const owner = await models.Owner.findOne({
+      where: {
+        id,
+      },
+    });
+    const pets = await owner.getPets();
+    res.send(pets);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// /* POST new owner. */
+// router.post('/', async function(req, res, next) {
+//   const { firstname, lastname, username, email, password } = req.body;
+//   try {
+//     const owner = await models.Owner.create({
+//       firstname, 
+//       lastname, 
+//       username, 
+//       email, 
+//       password
+//     });
+//     res.send(owner);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
+
+/* POST new pet associated to owner. */
+router.post('/:id/pets', async function(req, res, next) {
+  const { id } = req.params;
+  const { name, type, age, sex } = req.body;
+  try {
+    const owner = await models.Owner.findOne({
+      where: {
+        id,
+      },
+    });
+    const pet = await owner.createPet({
+      name, 
+      type, 
+      age, 
+      sex
+    });
+    res.send(pet);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+/* PUT existing owner. */
+router.put('/:id', async function(req, res, next) {
+  const { id } = req.params;
+  const { firstname, lastname, username, email, password } = req.body;
+  try {
+    const owner = await models.Owner.findOne({
+      where: {
+        id,
+      },
+    });
+
+    const updOwner = await owner.update({ firstname, lastname, username, email, password })
+    res.send(updOwner);
+
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/* DELETE one owner. */
+router.delete('/:id', async function(req, res, next) {
+  const { id } = req.params;
+  try {
+    await models.Owner.destroy({
+      where: {
+        id,
+      },
+    });
+
+    const owners = await models.Owner.findAll();
+    res.send(owners);
+
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/* GET one owner by pet?. */
+
+/* GET one owner by clinic?. */
 
 module.exports = router;
