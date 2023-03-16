78edaeaa15f0c111f94485e8d72f05df58db439e
Maya Tripathy <maya.tripathy1@gmail.com>
2023-03-16 15:20:08 +0100

    fixed typo on button

--- client/src/components/AllPetsList.js
+++ client/src/components/AllPetsList.js
@@ -83,7 +83,7 @@ export default function AllPetsList(props) {
                                 title="view"
                                 type="button"
                                 >
-                                veiw
+                                view
                             </Button> }
                         </td>
                     </tr>
