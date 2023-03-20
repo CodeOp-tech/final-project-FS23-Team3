76d9c21e9f6d0638ec761eba547c663c39cbfcc0
rese-tw <th.warzecha@gmail.com>
2023-03-20 22:46:11 +0100

    add vet to my vets, make appointment form for specific clinic

--- models/appointment.js
+++ models/appointment.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Appointment.belongsTo(models.Pet);
+      Appointment.belongsTo(models.Clinic);
     }
   }
   Appointment.init({
